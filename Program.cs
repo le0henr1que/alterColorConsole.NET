96afe041a4d3c50f3441c641c1a46a2053211999
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2022-11-19 21:01:22 -0300

    init project

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FluentColorConsole;
+
+Console.WriteLine("Hello, World!");
+
+var textLine = ColorConsole.WithBlueText;
+
+textLine.WriteLine("My text blue");
